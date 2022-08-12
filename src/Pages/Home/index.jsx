4c650689b3eb1a3e729ac34b5ac83aef12182d9f
Janilson Júnior <81278385+JanilsonJ@users.noreply.github.com>
2022-08-12 18:36:32 -0300

    Adicionando responsividade

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,12 +3,15 @@ import { useEffect } from 'react';
 import './home.css';
 
 const Home = () => {
+    
+    
     useEffect(() => {
         document.title = 'IMA - Início';
     });
 
     return (
         <section className='home'>
+            HOME PAGE
         </section>
     )
 }
