c782360f536ccee0dd3fb4ff5f85e705cd5dce7f
Janilson Júnior <81278385+JanilsonJ@users.noreply.github.com>
2022-08-16 16:58:28 -0300

    Imagens para demonstração

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,16 +3,19 @@ import { useEffect } from 'react';
 import './home.css';
 
 const Home = () => {
-    
-    
     useEffect(() => {
         document.title = 'IMA - Início';
     });
 
     return (
-        <section className='home'>
-            HOME PAGE
-        </section>
+        <>
+            <section className='home'>
+                <img src="/assets/img/Products/10_Vestido regata com estampados florais_frente.webp" alt="" />
+                <img src="/assets/img/Products/10_Vestido regata com estampados florais_frente.webp" alt="" />
+                <img src="/assets/img/Products/10_Vestido regata com estampados florais_frente.webp" alt="" />
+                <img src="/assets/img/Products/10_Vestido regata com estampados florais_frente.webp" alt="" />
+            </section>
+        </>
     )
 }
 
