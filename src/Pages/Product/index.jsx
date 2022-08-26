0602463a6b64bd1f3db494d360c1bbf3eaa1f850
Fabioazevedojr <fabioazevedojr@gmail.com>
2022-08-25 23:10:24 -0300

    Adicionando desconto no produto e ajustando botão de tamanho

--- src/Pages/Product/index.jsx
+++ src/Pages/Product/index.jsx
@@ -1,29 +1,136 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react'
+import { CartContext } from '../../Context/cart'
+import './product.css'
+import ProductList from '../../data/products'
 import { useParams } from 'react-router-dom'
 
-import { CartContext } from '../../Context/cart';
-import ProductsList from '../../data/products';
-
-import './product.css';
-
 const Product = () => {
-    const { addBagItem } = useContext(CartContext);
+  const { id } = useParams()
+  const product = ProductList.find(p => p.id === parseInt(id))
 
-    const { id } = useParams();
-    const product = ProductsList.find(p => p.id === parseInt(id));
+  const [mainImage, setMainImage] = useState(product.img.front)
 
-    useEffect(() => {
-        document.title = 'IMA - ' + product.name;
-    });
+  const { addBagItem } = useContext(CartContext)
 
-    return (
-        <>
-            <p>{product.name}</p>
-            
-            <img src={product.img.front} alt={product.name} style={{width: '300px'}}/>
-            <button onClick={() => { addBagItem(product) }}>Adicionar a sacola</button>
-        </>
-    )
+  function payment() {
+    return product.actual_price / 3
+  }
+
+  useEffect(() => {
+    document.title = `IMA - ${product.name}`
+  })
+
+  return (
+    <>
+      <div className="container">
+        <div className="products--images">
+          <div className="main--product--image">
+            <img id="image_test" src={mainImage} alt={product.name} />
+          </div>
+          <div className="image--product--preview">
+            <img
+              onClick={() => {
+                setMainImage(product.img.front)
+              }}
+              src={product.img.front}
+              alt={product.name}
+            />
+            <img
+              onClick={() => {
+                setMainImage(product.img.back)
+              }}
+              src={product.img.back}
+              alt={product.name}
+            />
+          </div>
+        </div>
+        <div className="info--product--container">
+          <p className="product--name">{product.name}</p>
+          <span
+            className="regular--price"
+            style={
+              product.porcent_descount !== 0
+                ? { display: 'unset' }
+                : { display: 'none' }
+            }
+          >
+            {' '}
+            De: R${' '}
+            {product.regular_price.toFixed(2).toString().replace('.', ',')}
+          </span>
+          <span
+            className="product--price"
+            style={
+              product.porcent_descount !== 0
+                ? { margin: '10px 0 0 0' }
+                : { margin: '120px 0 0 0' }
+            }
+          >
+            R$ {product.actual_price.toFixed(2).toString().replace('.', ',')}
+          </span>
+          <p className="payment--info">
+            em até 3x {payment().toFixed(2).toString().replace('.', ',')}
+          </p>
+
+          <div className="product--size">
+            <p>Escolha o tamanho:</p>
+            <button
+              style={
+                !product.sizes.PP
+                  ? { cursor: 'default', color: 'grey' }
+                  : { cursor: 'pointer' }
+              }
+            >
+              PP
+            </button>
+            <button
+              style={
+                !product.sizes.P
+                  ? { cursor: 'default', color: 'grey' }
+                  : { cursor: 'pointer' }
+              }
+            >
+              P
+            </button>
+            <button
+              style={
+                !product.sizes.M
+                  ? { cursor: 'default', color: 'grey' }
+                  : { cursor: 'pointer' }
+              }
+            >
+              M
+            </button>
+            <button
+              style={
+                !product.sizes.G
+                  ? { cursor: 'default', color: 'grey' }
+                  : { cursor: 'pointer' }
+              }
+            >
+              G
+            </button>
+            <button
+              style={
+                !product.sizes.GG
+                  ? { cursor: 'default' }
+                  : { cursor: 'pointer' }
+              }
+            >
+              GG
+            </button>
+          </div>
+          <button
+            onClick={() => {
+              addBagItem(product)
+            }}
+          >
+            Adicionar ao Carrinho
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Product
--- src/data/products.js
+++ src/data/products.js
@@ -1,202 +1,209 @@
-const imgPath = "/assets/img/Products/";
+const imgPath = '/assets/img/Products/'
 
 const products = [
-    {
-        id: 1,
-        name: "Blusa Verde Simples",
-        img: {
-            front: imgPath + "Produto01_frente.webp",
-            back: imgPath + "Produto01_verso.webp"
-        },
-        type: 'Blusa',
-        keywords: ['Blusa', 'Verde', 'Simples'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 59.90,
-        actual_price: 53.91,
-        porcent_descount: 10
+  {
+    id: 1,
+    name: 'Blusa Verde Simples',
+    img: {
+      front: imgPath + 'Produto01_frente.webp',
+      back: imgPath + 'Produto01_verso.webp'
     },
-    {
-        id: 2,
-        name: "Camiseta Gola Redonda",
-        img: {
-            front: imgPath + "Produto02_frente.webp",
-            back: imgPath + "Produto02_verso.webp"
-        },
-        type: 'Camiseta',
-        keywords: ['Camiseta', 'Gola Redonda', 'Verde'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 77.90,
-        actual_price: 70.11,
-        porcent_descount: 10
+    type: 'Blusa',
+    keywords: ['Blusa', 'Verde', 'Simples'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 59.9,
+    actual_price: 53.91,
+    porcent_descount: 10
+  },
+  {
+    id: 2,
+    name: 'Camiseta Gola Redonda',
+    img: {
+      front: imgPath + 'Produto02_frente.webp',
+      back: imgPath + 'Produto02_verso.webp'
     },
-    {
-        id: 3,
-        name: "Terno e blazer - Frontal aberto - Com Gola Xale",
-        img: {
-            front: imgPath + "Produto03_frente.webp",
-            back: imgPath + "Produto03_verso.webp"
-        },
-        type: 'Terno e blazer',
-        keywords: ['Terno', 'Blazer', 'Azul Marinho', 'Conjunto'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 186.90,
-        actual_price: 186.90,
-        porcent_descount: 0
+    type: 'Camiseta',
+    keywords: ['Camiseta', 'Gola Redonda', 'Verde'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 77.9,
+    actual_price: 70.11,
+    porcent_descount: 10
+  },
+  {
+    id: 3,
+    name: 'Terno e blazer - Frontal aberto - Com Gola Xale',
+    img: {
+      front: imgPath + 'Produto03_frente.webp',
+      back: imgPath + 'Produto03_verso.webp'
     },
-    {
-        id: 4,
-        name: "Camiseta Ocasional Plantas",
-        img: {
-            front: imgPath + "Produto04_frente.webp",
-            back: imgPath + "Produto04_verso.webp"
-        },
-        type: 'Camiseta',
-        keywords: ['Camiseta', 'Plantas', 'Verde'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 39.90,
-        actual_price: 39.90,
-        porcent_descount: 0
+    type: 'Terno e blazer',
+    keywords: ['Terno', 'Blazer', 'Azul Marinho', 'Conjunto'],
+    sizes: { PP: false, P: false, M: true, G: true, GG: true },
+    regular_price: 186.9,
+    actual_price: 186.9,
+    porcent_descount: 0
+  },
+  {
+    id: 4,
+    name: 'Camiseta Ocasional Plantas',
+    img: {
+      front: imgPath + 'Produto04_frente.webp',
+      back: imgPath + 'Produto04_verso.webp'
     },
-    {
-        id: 5,
-        name: "Camisa Listrada Formal - Masculina",
-        img: {
-            front: imgPath + "Produto05_frente.webp",
-            back: imgPath + "Produto05_verso.webp"
-        },
-        type: 'Camiseta',
-        keywords: ['Camisa', 'Listrada', 'Branca'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 76.90,
-        actual_price: 76.90,
-        porcent_descount: 0
+    type: 'Camiseta',
+    keywords: ['Camiseta', 'Plantas', 'Verde'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 39.9,
+    actual_price: 39.9,
+    porcent_descount: 0
+  },
+  {
+    id: 5,
+    name: 'Camisa Listrada Formal - Masculina',
+    img: {
+      front: imgPath + 'Produto05_frente.webp',
+      back: imgPath + 'Produto05_verso.webp'
     },
-    {
-        id: 6,
-        name: "Moletom de bolso com cordão",
-        img: {
-            front: imgPath + "Produto06_frente.webp",
-            back: imgPath + "Produto06_verso.webp"
-        },
-        type: 'Moletom',
-        keywords: ['Moletom', 'Verde', 'Capuz', 'Bolso'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 105.90,
-        actual_price: 90.02,
-        porcent_descount: 15
+    type: 'Camiseta',
+    keywords: ['Camisa', 'Listrada', 'Branca'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 76.9,
+    actual_price: 76.9,
+    porcent_descount: 0
+  },
+  {
+    id: 6,
+    name: 'Moletom de bolso com cordão',
+    img: {
+      front: imgPath + 'Produto06_frente.webp',
+      back: imgPath + 'Produto06_verso.webp'
     },
-    {
-        id: 7,
-        name: "Moletom Impressão gráfica Bloco de cores Cordão",
-        img: {
-            front: imgPath + "Produto07_frente.webp",
-            back: imgPath + "Produto07_verso.webp"
-        },
-        type: 'Moletom',
-        keywords: ['Moletom', 'Preta', 'Preto', 'Emoji', 'Capuz'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 106.90,
-        actual_price: 106.90,
-        porcent_descount: 0
+    type: 'Moletom',
+    keywords: ['Moletom', 'Verde', 'Capuz', 'Bolso'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 105.9,
+    actual_price: 90.02,
+    porcent_descount: 15
+  },
+  {
+    id: 7,
+    name: 'Moletom Impressão gráfica Bloco de cores Cordão',
+    img: {
+      front: imgPath + 'Produto07_frente.webp',
+      back: imgPath + 'Produto07_verso.webp'
     },
-    {
-        id: 8,
-        name: "Moletom Halloween - EXCUSE ME BUT BOO",
-        img: {
-            front: imgPath + "Produto08_frente.webp",
-            back: imgPath + "Produto08_verso.webp"
-        },
-        type: 'Moletom',
-        keywords: ['Moletom', 'Azul', 'Halloween'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 59.95,
-        actual_price: 59.95,
-        porcent_descount: 0
+    type: 'Moletom',
+    keywords: ['Moletom', 'Preta', 'Preto', 'Emoji', 'Capuz'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 106.9,
+    actual_price: 106.9,
+    porcent_descount: 0
+  },
+  {
+    id: 8,
+    name: 'Moletom Halloween - EXCUSE ME BUT BOO',
+    img: {
+      front: imgPath + 'Produto08_frente.webp',
+      back: imgPath + 'Produto08_verso.webp'
     },
-    {
-        id: 9,
-        name: "Blusa Branca",
-        img: {
-            front: imgPath + "Produto09_frente.webp",
-            back: imgPath + "Produto09_verso.webp"
-        },
-        type: 'Blusa',
-        keywords: ['Blusa', 'Branco', 'Branca'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 49.90,
-        actual_price: 49.90,
-        porcent_descount: 0
+    type: 'Moletom',
+    keywords: ['Moletom', 'Azul', 'Halloween'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 59.95,
+    actual_price: 59.95,
+    porcent_descount: 0
+  },
+  {
+    id: 9,
+    name: 'Blusa Branca',
+    img: {
+      front: imgPath + 'Produto09_frente.webp',
+      back: imgPath + 'Produto09_verso.webp'
     },
-    {
-        id: 10,
-        name: "Vestido regata com estampados florais",
-        img: {
-            front: imgPath + "Produto10_frente.webp",
-            back: imgPath + "Produto10_verso.webp"
-        },
-        type: 'Vestido',
-        keywords: ['Vestido', 'Branca', 'Branco', 'Florais', 'Regata'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 75.90,
-        actual_price: 68.31,
-        porcent_descount: 10
+    type: 'Blusa',
+    keywords: ['Blusa', 'Branco', 'Branca'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 49.9,
+    actual_price: 49.9,
+    porcent_descount: 0
+  },
+  {
+    id: 10,
+    name: 'Vestido regata com estampados florais',
+    img: {
+      front: imgPath + 'Produto10_frente.webp',
+      back: imgPath + 'Produto10_verso.webp'
     },
-    {
-        id: 11,
-        name: "Camisa botões na frente - Espaço",
-        img: {
-            front: imgPath + "Produto11_frente.webp",
-            back: imgPath + "Produto11_verso.webp"
-        },
-        type: 'Camisa',
-        keywords: ['Camisa', 'Botões', 'Espaço', 'Manga Curta'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 85.90,
-        actual_price: 85.90,
-        porcent_descount: 0
+    type: 'Vestido',
+    keywords: ['Vestido', 'Branca', 'Branco', 'Florais', 'Regata'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 75.9,
+    actual_price: 68.31,
+    porcent_descount: 10
+  },
+  {
+    id: 11,
+    name: 'Camisa botões na frente - Espaço',
+    img: {
+      front: imgPath + 'Produto11_frente.webp',
+      back: imgPath + 'Produto11_verso.webp'
     },
-    {
-        id: 12,
-        name: "Blusa - Escritas Japonesas",
-        img: {
-            front: imgPath + "Produto12_frente.webp",
-            back: imgPath + "Produto12_verso.webp"
-        },
-        type: 'Blusa',
-        keywords: ['Blusa', 'Preta', 'Preto', 'Escrita', 'Japão', 'Escritas Japonesas'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 85.90,
-        actual_price: 85.90,
-        porcent_descount: 0
+    type: 'Camisa',
+    keywords: ['Camisa', 'Botões', 'Espaço', 'Manga Curta'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 85.9,
+    actual_price: 85.9,
+    porcent_descount: 0
+  },
+  {
+    id: 12,
+    name: 'Blusa - Escritas Japonesas',
+    img: {
+      front: imgPath + 'Produto12_frente.webp',
+      back: imgPath + 'Produto12_verso.webp'
     },
-    {
-        id: 13,
-        name: "Vestido Floral Estampado",
-        img: {
-            front: imgPath + "Produto13_frente.webp",
-            back: imgPath + "Produto13_verso.webp"
-        },
-        type: 'Vestido',
-        keywords: ['Vestido', 'Floral', 'Preta', 'Preto', 'Regata'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 63.99,
-        actual_price: 63.99,
-        porcent_descount: 0
+    type: 'Blusa',
+    keywords: [
+      'Blusa',
+      'Preta',
+      'Preto',
+      'Escrita',
+      'Japão',
+      'Escritas Japonesas'
+    ],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 85.9,
+    actual_price: 85.9,
+    porcent_descount: 0
+  },
+  {
+    id: 13,
+    name: 'Vestido Floral Estampado',
+    img: {
+      front: imgPath + 'Produto13_frente.webp',
+      back: imgPath + 'Produto13_verso.webp'
     },
-    {
-        id: 14,
-        name: "Camisa com bolso ",
-        img: {
-            front: imgPath + "Produto14_frente.webp",
-            back: imgPath + "Produto14_verso.webp"
-        },
-        type: 'Camisa',
-        keywords: ['Camisa', 'Preta', 'Preto', 'Bolso', 'Manga Longa', 'Botão'],
-        sizes: { PP: true, P: true, M: true, G: true, GG: true },
-        regular_price: 105.99,
-        actual_price: 105.99,
-        porcent_descount: 0
-    }
+    type: 'Vestido',
+    keywords: ['Vestido', 'Floral', 'Preta', 'Preto', 'Regata'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 63.99,
+    actual_price: 63.99,
+    porcent_descount: 0
+  },
+  {
+    id: 14,
+    name: 'Camisa com bolso ',
+    img: {
+      front: imgPath + 'Produto14_frente.webp',
+      back: imgPath + 'Produto14_verso.webp'
+    },
+    type: 'Camisa',
+    keywords: ['Camisa', 'Preta', 'Preto', 'Bolso', 'Manga Longa', 'Botão'],
+    sizes: { PP: true, P: true, M: true, G: true, GG: true },
+    regular_price: 105.99,
+    actual_price: 105.99,
+    porcent_descount: 0
+  }
 ]
 
 module.exports = products
