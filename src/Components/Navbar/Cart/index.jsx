e38be525e9eef5e3be34ce9b1ae81c807b97220c
Artur Diego <arturdiegodev@gmail.com>
2022-08-26 21:39:15 -0300

    Adicionando lógica do Carrinho de Compras, Style do Carrinho, Img, MetallicRed Alterada

--- src/Components/Navbar/Cart/index.jsx
+++ src/Components/Navbar/Cart/index.jsx
@@ -1,27 +1,80 @@
-import { useContext } from 'react';
-
-import { CartContext } from '../../../Context/cart';
-
 import './cart.css';
+import React, { useContext } from 'react';
+import { FaMinusSquare, FaTrashAlt, FaPlusSquare } from "react-icons/fa";
+import { CartContext } from '../../../Context/cart';
+import { Link  } from 'react-router-dom'
 
-const Cart = () => {
-    const {bagItems, deleteBagItem, removeBagItem, addBagItem} = useContext(CartContext);
-
-    return (
-        <section className='cart'>
-            {bagItems.map( (items) => { 
-                return(
-                    <div key={items.id}  style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
-                        <img src={items.img.front} alt={items.name} style={{width: '100px'}} />
-                        <button onClick={() => deleteBagItem(items)}> delete </button>
-                        <button onClick={() => removeBagItem(items)}> - </button>
-                        <p> {items.bagQuantity} </p>
-                        <button onClick={() => addBagItem(items)}> + </button>
+    export default function Cart () {
+        const { bagItems, addBagItem, onRemove, setBagItems, updateItemsCount, bagItemsCount } = useContext(CartContext)
+        const itemsValue = bagItems.reduce((a, c) => a + c.quantity * c.actual_price, 0);
+        const frete = itemsValue > 250 ? 0  : 25;
+        const totalValue = itemsValue + frete;
+    
+        return (
+            <aside>
+            <section className="cart__products">
+              {bagItems.length === 0 && <div><img className='empty-cart' src="/assets/img/Products/empty-cart.jpg" alt="empty-cart"/>
+                <p className='cart__text'>Seu carrinho está vazio!</p></div>}
+              
+              {bagItems.map((item) => (
+                <div key={item.id} className="cart__product-description">
+                  <img className='cart__product-image' key={item.id} src={item.img.front} alt={item.name} style={{width: '130px'}} />
+                  <div className="cart__product-name">{item.name}
+                  <div className='regular_price' style={item.porcent_descount !== 0
+                                                        ? {display: 'unset'}
+                                                        : {display: 'none'}}
+                  >R$ {item.regular_price.toFixed(2).toString().replace('.', ',')}</div>
+                  <div className="cart_product-price">
+                    {item.quantity} x R$ {item.actual_price.toFixed(2).toString().replace('.', ',')}
+                  </div>
+                    <div className="minus__plus">
+                        <FaMinusSquare onClick={() => onRemove(item)} className="minus" size="1.6rem" color= "var(--DustyGray)" />
+                          {' '}
+                        <FaPlusSquare onClick={() => addBagItem(item)} className="plus" size="1.6rem" color= "var(--DustyGray)" />
                     </div>
-                )
-            })}
-        </section>
-    )
-}
+                    <button onClick={() => {updateItemsCount(bagItemsCount - item.quantity)
+                                            setBagItems(bagItems.filter(p => p !== item))
+                                            if (bagItemsCount === 1)
+                                              updateItemsCount(null)
+                                        }
+                                    } 
+                              className="cart__product-remove"> <FaTrashAlt/> Remover</button>
+                  </div>
+                  
+                </div>
+              ))}
 
-export default Cart
+                  {bagItems.length !== 0 && (
+                
+                <section className="resume">
+                  {/* <div className="item__price">
+                    <div className="item__price-fix">Preço dos Items</div>
+                    <div className="titem__price-total">R$ {itemsValue.toFixed(2).toString().replace('.', ',')}</div>
+                  </div>
+                  <span className='cart__product-line'></span> */}
+                  <div className="total__value">
+                    <div className="total__value-fix">
+                      <strong>Preço total</strong>
+                    </div>
+                    <div className="total__value-p">
+                      <strong>R$ {totalValue.toFixed(2).toString().replace('.', ',')}</strong>
+                    </div>
+                  </div>
+                  <span className='cart__product-line'></span>
+                  <div className="frete__price">
+                    <div className="frete__price-fix">Valor do frete</div>
+                    <div className="frete__total-value">
+                      R$ {frete.toFixed(2).toString().replace('.', ',')}
+                    </div>
+                  </div>
+               
+                  <div>
+                  <Link to="/checkout" className="checkout-button">Ir Para Pagamento</Link>
+                  </div>
+                </section>
+              )} 
+                             
+            </section>          
+          </aside>
+        );
+      }
--- src/Context/cart.js
+++ src/Context/cart.js
@@ -1,39 +1,57 @@
-import { createContext, useState } from "react";
+import { useEffect } from 'react';
+import { createContext, useState } from 'react';
 
 export const CartContext = createContext();
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
     const [bagItems, setBagItems] = useState([]);
     const [bagItemsCount, updateItemsCount] = useState(null);
 
+    
     const addBagItem = (product) => {
-        if ( bagItems.includes(product) ){
-            product.bagQuantity++;
-            updateItemsCount(bagItemsCount + 1);
-        } else{
-            product.bagQuantity = 1;
-            setBagItems([...bagItems, product]);
-            updateItemsCount(bagItemsCount + 1);
+        const Exist = bagItems.find((x) => x.id === product.id);
+        if (Exist) {
+            const newBagItems = bagItems.map((x) =>
+                x.id === product.id
+                    ? { ...Exist, quantity: Exist.quantity + 1 }
+                    : x
+            );
+            setBagItems(newBagItems);
+        } else {
+            const newBagItems = [...bagItems, { ...product, quantity: +1 }];
+            setBagItems(newBagItems);
         }
-    }
+        updateItemsCount(bagItemsCount + 1);
+    };
 
-    const removeBagItem = (product) => {
-        product.bagQuantity === 1 ? deleteBagItem(product) : product.bagQuantity--;
+    const onRemove = (product) => {
+        const Exist = bagItems.find((x) => x.id === product.id);
+        if (Exist.quantity === 1) {
+            setBagItems(bagItems.filter((p) => p !== product));
+        } else {
+            const newBagItems = bagItems.map((x) =>
+                x.id === product.id
+                    ? { ...Exist, quantity: Exist.quantity - 1 }
+                    : x
+            );
+            setBagItems(newBagItems);
+        }
         updateItemsCount(bagItemsCount - 1);
-    }
-
-    const deleteBagItem = (product) => {
-        bagItemsCount - product.bagQuantity === 0 ? 
-            updateItemsCount(null)
-            :
-            updateItemsCount(bagItemsCount - product.bagQuantity);
-
-        setBagItems(bagItems.filter((p) => p !== product))
-    }
+        if (bagItemsCount === 1) updateItemsCount(null);
+    };
 
     return (
-        <CartContext.Provider value={{bagItems, setBagItems, removeBagItem, addBagItem, bagItemsCount, deleteBagItem}}>
-            { children }
+        <CartContext.Provider
+            value={{
+                bagItems,
+                setBagItems,
+                onRemove,
+                addBagItem,
+                bagItemsCount,
+                updateItemsCount,
+            }}
+        >
+            {children}
         </CartContext.Provider>
-    )
-}
+    );
+};
