3f212fa7453e2fef97c8e19c741d3469b6921748
Janilson Júnior <81278385+JanilsonJ@users.noreply.github.com>
2022-09-02 13:11:39 -0300

    Adicionando filtro na página home a partir da url

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,17 +2,19 @@ import { useEffect } from 'react';
 import Catalago from '../../Components/Catalago';
 import './home.css';
 import BannerTopo from '../../Components/Catalago/Banners';
+import { useParams } from 'react-router-dom';
 
 const Home = () => {
     useEffect(() => {
         document.title = 'IMA - Início';
     });
+  
+    const { filter } = useParams()
 
     return (
             
             <section className='home'>
-               
-               <BannerTopo /> 
+               {filter ? null : <BannerTopo /> } 
                
                <Catalago />
                 
