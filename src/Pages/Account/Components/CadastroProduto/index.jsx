a56e2157d24d70609fad319ec8fdcbc255055503
Janilson Júnior <81278385+JanilsonJ@users.noreply.github.com>
2022-10-05 00:30:33 -0300

    Tela de adição de produtos

--- src/Pages/Account/Components/CadastroProduto/index.jsx
+++ src/Pages/Account/Components/CadastroProduto/index.jsx
@@ -3,8 +3,16 @@ import CampoTexto from '../CampoTexto'
 import Button from '../../../../Components/Button';
 
 import './cadastroProduto.css';
+import { useState } from 'react';
+import useFetch from '../../../../hooks/useFetch';
+import LoadBar from '../../../../Components/LoadBar';
 
 const CadastroProduto = () => {
+    const categoryOptions = ["Bermuda", "Blusa", "Camisa", "Camiseta", "Conjunto", "Moletom", "Vestido", "Short"]
+
+    const [showNewProductForm, setShowNewProductForm] = useState(false);
+
+    const { data: products, isFetching: loadProducts } = useFetch('products'); // Pegando Produtos na API
 
     const newProduct = async (e) => {
         e.preventDefault();
@@ -54,45 +62,97 @@ const CadastroProduto = () => {
         }); 
     }
 
-    return (
-        <div>
-            <section className="formulario">
-                <form className="Form__CadastroProduto" onSubmit={newProduct}>
-                    <h2>Cadastrar novo produto</h2>
-                    <CampoTexto label="Nome" placeholder="Digite o nome do produto" name='name' required/>
-                    <CampoTexto label="Descrição" placeholder="Digite o nome do produto" name='description'/>
-                    
-                    <select name="category" id="category">
-                        <option value="Bermuda">Bermuda</option>
-                        <option value="Blusa">Blusa</option>
-                        <option value="Camisa">Camisa</option>
-                        <option value="Camiseta">Camiseta</option>
-                        <option value="Conjunto">Conjunto</option>
-                        <option value="Moletom">Moletom</option>
-                        <option value="Vestido">Vestido</option>
-                        <option value="Short">Short</option>
-                    </select>
+    const updateProdutctForm = (product) => {
+        console.log(product)
+        return (
+            <div>
+                <div className="formulario">
+                    <form className="Form__CadastroProduto" onSubmit={newProduct}>
+                        <h2>Cadastrar novo produto</h2>
+                        <CampoTexto label="Nome" placeholder="Digite o nome do produto" name='name' defaultValue={product.name} required/>
+                        <CampoTexto label="Descrição" placeholder="Digite o nome do produto" name='description' defaultValue={product.description} />
+                        <CampoTexto label="Categoria" type='select' name="category" id="category" selectOptions={categoryOptions}/>
+                        <CampoTexto label="Imagem - 1 (URL)" name='image1' type="urlImage" defaultValue={product.image1} required/>
+                        <CampoTexto label="Imagem - 2 (URL)" name='image2' type="urlImage" defaultValue={product.image2} required/>
+                        <CampoTexto label="Preço regular" name='regular_price' type="number" defaultValue={product.regular_price} step="0.01" required/>
+                        <CampoTexto label="Preço atual" name='actual_price' type="number" defaultValue={product.actual_price} step="0.01" required/>
+                        <CampoTexto label="Desconto(%)" name='porcent_discount' type="number" defaultValue={product.porcent_discount} required/>
 
-                    <CampoTexto label="Imagem - 1 (URL)" name='image1' type="url" required/>
-                    <CampoTexto label="Imagem - 2 (URL)" name='image2' type="url" required/>
-                    <CampoTexto label="Preço regular" placeholder="R$" name='regular_price' type="number" step="0.01" required/>
-                    <CampoTexto label="Preço atual" placeholder="R$" name='actual_price' type="number" step="0.01" required/>
-                    <CampoTexto label="Desconto(%)" placeholder="%" name='porcent_discount' type="number" step="0.01" required/>
+                        <h3 style={{marginBottom: '15px', textAlign: 'center'}}>Quantidade em estoque para cada tamanho</h3>
+                        <CampoTexto name='PP' label="Tamanho PP:" size="50" type="number" min="0" required/>
+                        <CampoTexto name='P'label="Tamanho P:" type="number" min="0" required/>
+                        <CampoTexto name='M' label="Tamanho M:" type="number" min="0" required/>
+                        <CampoTexto name='G' label="Tamanho G:" type="number" min="0" required/>
+                        <CampoTexto name='GG' label="Tamanho GG:" type="number" min="0" required/>
+                        
+                        <div className='formulario__botao'>
+                            <Button type="submit">Adicionar novo produto</Button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
 
-                    <h3 style={{marginBottom: '15px', textAlign: 'center'}}>Quantidade em estoque para cada tamanho</h3>
-                    <CampoTexto name='PP' label="Tamanho: PP" type="number" min="0" required/>
-                    <CampoTexto name='P'label="Tamanho: P" type="number" min="0" required/>
-                    <CampoTexto name='M' label="Tamanho: M" type="number" min="0" required/>
-                    <CampoTexto name='G' label="Tamanho: G" type="number" min="0" required/>
-                    <CampoTexto name='GG' label="Tamanho: GG" type="number" min="0" required/>
-                    
-                    <div className='formulario__botao'>
-                        <Button type="submit">Salvar Alterações</Button>
+    const allProducts = () => {
+        if (loadProducts) 
+            return <LoadBar title="Carregando Produtos..." />
+        else {
+            return (
+                <>
+                    <div className="product">
+                        <button onClick={() => setShowNewProductForm(true)}>Adicionar Produto</button>
                     </div>
-                </form>
-            </section>
-        </div>
-    )
+                    {
+                        products.map(product => {
+                            return( 
+                                <div key={product.id} className="product">
+                                    <h3>{product.name}</h3>
+                                    <hr /> 
+                                    <img className="item_img" src={product.image1} alt={product.name} />
+                
+                                    <button onClick={() => updateProdutctForm(product)}>Editar Produto</button>
+                                </div>
+                            )
+                        })
+                    }
+                </>
+            )
+        }
+    }
+
+    const newProductForm = () => {
+        return (
+            <div>
+                <div className="formulario">
+                    <form className="Form__CadastroProduto" onSubmit={newProduct}>
+                        <h2>Cadastrar novo produto</h2>
+                        <CampoTexto label="Nome" placeholder="Digite o nome do produto" name='name' required/>
+                        <CampoTexto label="Descrição" placeholder="Digite o nome do produto" name='description'/>
+                        <CampoTexto label="Categoria" type='select' name="category" id="category" selectOptions={categoryOptions}/>
+                        <CampoTexto label="Imagem - 1 (URL)" name='image1' type="urlImage" required/>
+                        <CampoTexto label="Imagem - 2 (URL)" name='image2' type="urlImage" required/>
+                        <CampoTexto label="Preço regular" name='regular_price' type="number" step="0.01" required/>
+                        <CampoTexto label="Preço atual" name='actual_price' type="number" step="0.01" required/>
+                        <CampoTexto label="Desconto(%)" name='porcent_discount' type="number" required/>
+    
+                        <h3 style={{marginBottom: '15px', textAlign: 'center'}}>Quantidade em estoque para cada tamanho</h3>
+                        <CampoTexto name='PP' label="Tamanho PP:" size="50" type="number" min="0" required/>
+                        <CampoTexto name='P'label="Tamanho P:" type="number" min="0" required/>
+                        <CampoTexto name='M' label="Tamanho M:" type="number" min="0" required/>
+                        <CampoTexto name='G' label="Tamanho G:" type="number" min="0" required/>
+                        <CampoTexto name='GG' label="Tamanho GG:" type="number" min="0" required/>
+                        
+                        <div className='formulario__botao'>
+                            <Button type="submit">Adicionar novo produto</Button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+    return showNewProductForm ? newProductForm() : allProducts();
 }
 
 export default CadastroProduto
